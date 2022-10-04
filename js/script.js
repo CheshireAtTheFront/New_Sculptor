252a83f9cb9a82babf19e05db8de74a080695d6f
Диана Макарова <pr_dev_01@office.df-mdf.com>
2022-10-04 16:55:06 +0300

    added a stylized block of rules

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let rules = document.querySelector("#rules");
+
+rules.addEventListener("click", () => {
+    console.log("клик");
+    // if (rules) {
+    //     document.querySelector('#iframe-rules').style = "display: block;"
+    // }
+    document.querySelector('#iframe-rules').style = "display: block;"
+})
+
+// let iframeRules =
