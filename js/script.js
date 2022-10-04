a6cd5540a80b01c45eb238234af802947e0d1a75
CheshireAtTheFront <DIN_M-v@mail.ru>
2022-10-04 23:28:27 +0300

    I corrected the block of rules and wrote a script in js to close and open the block

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
-let rules = document.querySelector("#rules");
+function funRules() {
+	if (rules = true) {
+	let rules = document.getElementById('rules-js');
+	// let iframe = document.getElementById('iframe-rules');
+			rules.addEventListener("click", () => {
+			document.getElementById('iframe-rules').style = "display: block;"
 
-rules.addEventListener("click", () => {
-    console.log("клик");
-    // if (rules) {
-    //     document.querySelector('#iframe-rules').style = "display: block;"
-    // }
-    document.querySelector('#iframe-rules').style = "display: block;"
-})
+		});
+	} 
+		if (close = true) {
+			let close = document.getElementById('close-js');
+			close.addEventListener("click", () => {
+			document.getElementById('iframe-rules').style = "display: none;"
+			});
+	
+		}
+}
+funRules();
+
+// rules
 
 // let iframeRules =
