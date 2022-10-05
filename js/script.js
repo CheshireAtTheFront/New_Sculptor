20c5927789f8f75bdf6ac6de1a1027040baaf34c
Диана Макарова <pr_dev_01@office.df-mdf.com>
2022-10-05 16:18:15 +0300

    the news file was deleted, the adaptive was fixed, animation for trainers was added

--- js/script.js
+++ js/script.js
@@ -1,22 +1,52 @@
+// let rules = document.getElementById('rules-js');
+// let close = document.getElementById('close-js');
+// let iframe = document.getElementById('iframe-rules');
+
+// rules.addEventListener("click", () => {
+//     iframe.style = "display: block;"
+// });
+// close.addEventListener("click", () => {
+//     iframe.style = "display: none;"
+// });
+
 let rules = document.getElementById('rules-js');
 let close = document.getElementById('close-js');
 let iframe = document.getElementById('iframe-rules');
 
-rules.addEventListener("click", () => {
-    iframe.style = "display: block;"
-});
-close.addEventListener("click", () => {
-    iframe.style = "display: none;"
-});
+rules.addEventListener("click", funOpen);
+close.addEventListener("click", funClose);
 
-
-
-if (rules = true) {
-    // let rules = document.getElementById('rules-js');
-    // let iframe = document.getElementById('iframe-rules');
-
-} else if (close = true) {
-    // let close = document.getElementById('close-js');
-
-
+function funOpen() {
+    if (rules = true) {
+        iframe.style = "display: block;";
+    }
 }
+
+function funClose() {
+    iframe.style = "display: none;"
+}
+
+// rules.addEventListener("click", () => {
+//     let onclick = rules;
+//     let closeIframe = iframe;
+//     if (onclick = true) {
+//         closeIframe.style = "display: block;"
+//     }
+//     iframe.style = "display: none;"
+// });
+
+// close.addEventListener("click", () => {
+//     iframe.style = "display: none;"
+// });
+
+
+
+// if (rules = true) {
+//     // let rules = document.getElementById('rules-js');
+//     // let iframe = document.getElementById('iframe-rules');
+
+// } else if (close = true) {
+//     // let close = document.getElementById('close-js');
+
+
+// }
