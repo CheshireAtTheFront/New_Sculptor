266f8e0125f3e72dc73d2b04d93db64b82efa780
Диана Макарова <pr_dev_01@office.df-mdf.com>
2022-11-04 15:42:00 +0300

    add icon close and fixed file script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,3 @@
-// let rules = document.getElementById('rules-js');
-// let close = document.getElementById('close-js');
-// let iframe = document.getElementById('iframe-rules');
-
-// rules.addEventListener("click", () => {
-//     iframe.style = "display: block;"
-// });
-// close.addEventListener("click", () => {
-//     iframe.style = "display: none;"
-// });
-
 let rules = document.getElementById('rules-js');
 let close = document.getElementById('close-js');
 let iframe = document.getElementById('iframe-rules');
@@ -25,28 +14,3 @@ function funOpen() {
 function funClose() {
     iframe.style = "display: none;"
 }
-
-// rules.addEventListener("click", () => {
-//     let onclick = rules;
-//     let closeIframe = iframe;
-//     if (onclick = true) {
-//         closeIframe.style = "display: block;"
-//     }
-//     iframe.style = "display: none;"
-// });
-
-// close.addEventListener("click", () => {
-//     iframe.style = "display: none;"
-// });
-
-
-
-// if (rules = true) {
-//     // let rules = document.getElementById('rules-js');
-//     // let iframe = document.getElementById('iframe-rules');
-
-// } else if (close = true) {
-//     // let close = document.getElementById('close-js');
-
-
-// }
